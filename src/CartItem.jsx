4762aa30ed618c65923159ae518b5acf0f18c501
ibgkraft <ibacaicoa@hotmail.com>
2024-09-22 07:54:58 -0400

    XXX

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    Cart.forEach((item) => {
+        totalAmount += item.quantity;
+      });
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
    
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+    };
 
   const handleIncrement = (item) => {
+        dispatch(updateQuantity(item));
   };
 
   const handleDecrement = (item) => {
-   
+        dispatch(updateQuantity(item));
   };
 
   const handleRemove = (item) => {
+        dispatch(removeItem(item))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+    Cart.forEach((item) => {
+        totalCost += item.cost * item.quantity;
+      });
+    return totalCost;
   };
 
   return (
